2a9d3d4136ab1002310b243e81a88886611c34bc
Diogo V. Kersting <diogovk@gmail.com>
2015-12-04 17:32:02 -0200

    Vagrant configuration

---
+++ Vagrantfile
@@ -0,0 +1,10 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.provision "shell", path: "pg_config.sh"
+  # config.vm.box = "hashicorp/precise32"
+  config.vm.box = "ubuntu/trusty32"
+end
---
+++ pg_config.sh
@@ -0,0 +1,13 @@
+apt-get -qqy update
+apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-pip
+pip install bleach
+pip install requests
+pip install passlib
+pip install itsdangerous
+su postgres -c 'createuser -dRS vagrant'
+su vagrant -c 'createdb'
+
+vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
+echo -e $vagrantTip > /etc/motd
+
